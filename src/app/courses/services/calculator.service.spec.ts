8be1bfb39deccb41f9734eec76c1eb4672eef1ce
John Eatwell <>
2025-02-19 07:35:00 +0200

    added TestBed for dependency injection

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,5 +1,6 @@
 import { CalculatorService } from "./calculator.service";
 import { LoggerService } from "./logger.service";
+import { TestBed } from '@angular/core/testing'
 
 // Define Test Suite
 describe('CalculatorService', () => {
@@ -13,7 +14,14 @@ describe('CalculatorService', () => {
         console.log("Before Each setup");
         loggerSpy = jasmine.createSpyObj('LoggerService', ["log"]);
 
-        calculator = new CalculatorService(loggerSpy);
+        TestBed.configureTestingModule({
+            providers: [
+                CalculatorService,
+                { provide: LoggerService, useValue: loggerSpy }
+            ]
+        })
+
+        calculator = TestBed.inject(CalculatorService);
     });
 
     it('should add two numbers', () => {
